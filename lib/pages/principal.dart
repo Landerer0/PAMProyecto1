b7820f0d895a0f9e0b35643ee78143ea306e7bbe
Cris <pvasquez.cris@gmail.com>
2022-10-21 11:38:51 -0300

    sidebarx

--- lib/pages/principal.dart
+++ lib/pages/principal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
 
 import '../global.dart';
 
@@ -11,8 +12,20 @@ class Principal extends StatelessWidget {
         appBar: AppBar(
           title: Text("Principal ${Global.login}"),
         ),
+        //drawer: SidebarCustom(),
         body: Center(
           child: Text("ඞඞඞඞඞඞඞ login success"),
         ));
   }
+
+  /*
+  Widget SidebarCustom() {
+    return SidebarX(
+        controller: SidebarXController(selectedIndex: 0),
+        items: const [
+          SidebarXItem(icon: Icons.home, label: 'Home'),
+          SidebarXItem(icon: Icons.search, label: 'Search'),
+        ]);
+  }
+  */
 }
